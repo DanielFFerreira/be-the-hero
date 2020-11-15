8ae814cb36920ae867c13854bba48bbc382efa53
DanielFerreira <danielfferreirabr@gmail.com>
2020-11-15 20:48:20 -0300

    feat: Finished style of the registration page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Login from "./pages/login";
+import Profile from "./pages/profile";
 import Register from "./pages/register";
 
 export default function Routes() {
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile}/>
       </Switch>
     </BrowserRouter>
   );
