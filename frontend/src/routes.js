412b1b718ac67f703381ffc584a2ff4d367fa14d
DanielFerreira <danielfferreirabr@gmail.com>
2020-11-16 10:22:05 -0300

    feat: Page style finalized registration

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
 import Register from "./pages/register";
+import NewIncident from "./pages/newIncident";
 
 export default function Routes() {
   return (
@@ -12,6 +13,7 @@ export default function Routes() {
         <Route path="/" exact component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/profile" component={Profile}/>
+        <Route path="/incidents/new" component={NewIncident}/>
       </Switch>
     </BrowserRouter>
   );
