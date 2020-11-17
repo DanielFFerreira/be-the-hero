3a497848f0e478936ef7e4223ffd7ab2d2b7b7d9
DanielFerreira <danielfferreirabr@gmail.com>
2020-11-17 00:31:07 -0300

    feat: Ended connection with api

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import logoImg from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import api from '../../services/api';
 import './style.css';
 
 
 export default function Profile() {
+  const history = useHistory();
   const [incidents, setIncidents] = useState([]);
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
@@ -34,6 +35,11 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return(
     <div className="profile-container">
       <header>
@@ -45,7 +51,7 @@ export default function Profile() {
           Cadastrar novo caso
         </Link>
 
-        <button type="button">
+        <button onClick={handleLogout} type="button">
           <FiPower sixe={18} color="#E02041" />
         </button>
       </header>
